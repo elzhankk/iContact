514b438e269ee65183f9ac59041c0069687eca43
elzhankk <elzhan-96@mail.ru>
2023-11-23 06:32:07 +0600

    Initial Commit

---
+++ iContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iContact
+//
+//  Created by elzhankk on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
