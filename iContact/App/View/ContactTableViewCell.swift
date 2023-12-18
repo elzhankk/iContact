8b5658e0c15db0b6369a8cb9062fd7e4b9519903
elzhankk <elzhan-96@mail.ru>
2023-12-18 16:25:45 +0600

    Version 1.0

---
+++ iContact/App/View/ContactTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactTableViewCell.swift
+//  iContact
+//
+//  Created by elzhankk on 23.11.2023.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iContact/Manager/Contact Manager.swift
@@ -0,0 +1,29 @@
+//
+//  Contact Manager.swift
+//  iContact
+//
+//  Created by elzhankk on 23.11.2023.
+//
+
+import UIKit
+
+class Contact_Manager: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
